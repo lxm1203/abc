55c0b8a1072f02b6c411a663ab8d93024e289c80
lxm1203 <452279271@qq.com>
2022-05-28 16:37:59 +0800

    修改

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme";
 
-const userBase = 'blog';
+const userBase = 'abc';
 
 export default defineUserConfig({
   lang: "zh-CN",
